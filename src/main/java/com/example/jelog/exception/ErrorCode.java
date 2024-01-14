a3fc0cf5609cfb7e720af6d4c6dae253c9bdf1ae
sarumani <qkre2018@gmail.com>
2024-01-14 17:46:39 +0900

    새로운 에러코드 추가

--- src/main/java/com/example/jelog/exception/ErrorCode.java
+++ src/main/java/com/example/jelog/exception/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
     USEREMAIL_DUPLICATED(HttpStatus.CONFLICT, "중복된 이메일"),
     USEREMAIL_NOTEXIST(HttpStatus.NOT_FOUND, "존재하지 않는 이메일"),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "잘못된 비밀번호"),
-    POSTS_NOTEXIST(HttpStatus.NOT_FOUND, "포스트가 없습니다.");
+    POSTS_NOTEXIST(HttpStatus.NOT_FOUND, "포스트가 없습니다."),
+    WRONG_ACCEPT(HttpStatus.BAD_REQUEST, "잘못된 접근");
 
     private HttpStatus httpStatus;
     private String message;
