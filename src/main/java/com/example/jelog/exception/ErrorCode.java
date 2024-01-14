567ef5883e0383be54e716db53e6c91709239b1c
sarumani <qkre2018@gmail.com>
2024-01-14 22:13:04 +0900

    -

--- src/main/java/com/example/jelog/exception/ErrorCode.java
+++ src/main/java/com/example/jelog/exception/ErrorCode.java
@@ -7,10 +7,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    USEREMAIL_DUPLICATED(HttpStatus.CONFLICT, "중복된 이메일"),
-    USEREMAIL_NOTEXIST(HttpStatus.NOT_FOUND, "존재하지 않는 이메일"),
+    USER_EMAIL_DUPLICATED(HttpStatus.CONFLICT, "중복된 이메일"),
+    USER_DONT_EXIST(HttpStatus.NOT_FOUND, "존재하지 않는 이메일"),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "잘못된 비밀번호"),
-    POSTS_NOTEXIST(HttpStatus.NOT_FOUND, "포스트가 없습니다."),
+    POSTS_DONT_EXIST(HttpStatus.NOT_FOUND, "포스트가 없습니다."),
     WRONG_ACCEPT(HttpStatus.BAD_REQUEST, "잘못된 접근");
 
     private HttpStatus httpStatus;
